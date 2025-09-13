2d3f7da31075013a93f16b33b60a19bc621f1a8f
CordBase <144811064+cordbase@users.noreply.github.com>
2025-09-13 12:46:54 +0530

    Update infinityx_pacman.sh

--- infinityx_pacman.sh
+++ infinityx_pacman.sh
@@ -144,9 +144,7 @@ export PRODUCT_SYSTEM_PROPERTIES+=" \
     ro.infinity.maintainer=$INFINITY_MAINTAINER \
 "
 echo "======== flags setup complete ========"
-rm -rf build/make/tools/compliance
-rm -rf build/make/tools/rbcrun
-rm -rf build/make/tools/fs_config
+
 echo "======== Environment setup ========"
 . build/envsetup.sh
 echo "======== Environment setup complete ========"
