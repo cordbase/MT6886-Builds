6ade4f186a9cb864c451d0ff24b8d626dee15291
CordBase <144811064+cordbase@users.noreply.github.com>
2025-09-30 01:53:34 +0530

    Update infinityx_pacman.sh

--- infinityx_pacman.sh
+++ infinityx_pacman.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
-# Infinity X Build Script + Build Script for Crave
+# Clover Bringup + Build Script for Crave
 
-# Remove old directories
-rm -rf prebuilts/clang/host/linux-x86
-rm -rf out/soong/.intermediates/system/sepolicy
+# Cleanup
 rm -rf device/nothing/Aerodactyl
 rm -rf device/nothing/Aerodactyl-kernel
 rm -rf vendor/nothing/Aerodactyl
@@ -14,22 +12,18 @@ rm -rf hardware/mediatek
 rm -rf hardware/nothing
 rm -rf kernel/nothing/mt6886
 rm -rf kernel/nothing/mt6886-modules
-rm -rf hardware/lineage_compat
-rm -rf device/nothing/Aerodactyl-ntcamera
-rm -rf vendor/nothing/Aerodactyl-ntcamera
-rm -rf hardware/lineage_compat
 
-echo "======== Initializing repo ========"
+# Init Rom Manifest
 repo init --depth=1 --no-repo-verify --git-lfs -u https://github.com/ProjectInfinity-X/manifest -b 16 -g default,-mips,-darwin,-notdefault
-echo "======== Syncing sources (Crave optimized) ========"
-/opt/crave/resync.sh
 
-rm -rf hardware/qcom/sdm845
+# Sync the repositories  
+/opt/crave/resync.sh 
 
-echo "======== Adding Trees ========"
+#Cleanup
+rm -rf hardware/lineage/interfaces/sensors
 
 # device tree bringup
-git clone --branch infinityx https://github.com/cordbase/android_device_nothing_Aerodactyl.git device/nothing/Aerodactyl
+git clone --branch clover-A16 https://github.com/cordbase/android_device_nothing_Aerodactyl.git device/nothing/Aerodactyl
 
 # vendor bringup
 git clone --branch lineage-23.0 https://gitlab.com/nothing-2a/proprietary_vendor_nothing_Aerodactyl.git vendor/nothing/Aerodactyl
@@ -46,9 +40,8 @@ git clone --branch lineage-23.0 https://github.com/Nothing-2A/android_device_not
 git clone https://github.com/Nothing-2A/android_kernel_nothing_mt6886.git kernel/nothing/mt6886
 git clone https://github.com/Nothing-2A/android_kernel_modules_nothing_mt6886.git kernel/nothing/mt6886-modules
 
-# Nothing Camera bringup
-git clone https://github.com/Nothing-2A/android_device_nothing_Aerodactyl-ntcamera.git device/nothing/Aerodactyl-ntcamera
-git clone --branch lineage-23.0 https://github.com/cordbase/proprietary_vendor_nothing_Aerodactyl-ntcamera.git vendor/nothing/Aerodactyl-ntcamera
+# Dolby BringUP
+git clone --branch Dolby-Vision-1.1 https://github.com/swiitch-OFF-Lab/hardware_dolby.git hardware/dolby
 
 # set username
 git config --global user.name "cordbase"
@@ -58,7 +51,6 @@ git config --global user.email "cordbase@users.noreply.github.com"
 PATCHES=(
   "packages/apps/Aperture|36c9507ecf2a1a798d2e7931d9019bacc3cc6052|https://github.com/Nothing-2A/android_packages_apps_Aperture"
   "hardware/lineage/compat|60729c841a8b447896aa8108d2c0cfc0a5327041|https://github.com/LineageOS/android_hardware_lineage_compat"
-  "frameworks/base|79b3ae0b06ffdbadde3d2106a2bbf895b074ffb2|https://github.com/Nothing-2A/android_frameworks_base"
   "system/core|8ff6e7a68523c3b870d8dcd5713c71ea15b43dd2|https://github.com/Nothing-2A/android_system_core"
   "system/core|0d5990a96c5e6a404887f5575c5d00bcbbaaef74|https://github.com/Nothing-2A/android_system_core"
   "frameworks/base|f89e8fa592233d86ad2cabf81df245c4003587cb|https://github.com/AxionAOSP/android_frameworks_base"
@@ -99,19 +91,17 @@ for entry in "${PATCHES[@]}"; do
   popd > /dev/null
 done
 
-echo -e "\n[✔] All patches processed!"
+echo -e "All patches processed!"
 
-echo "===========All repositories cloned successfully!==========="
+# Variables
+export BUILD_USERNAME=Himanshu
+export BUILD_HOSTNAME=crave
 
-echo "======== flags setup complete ========"
-
-echo "======== Environment setup ========"
+# Set up build environment
 . build/envsetup.sh
-echo "======== Environment setup complete ========"
-# ──────────────────────────────
-# Lunch & Build
-# ──────────────────────────────
-echo "======== Lunching target ========"
-lunch infinity_Pacman-user
+
+# lunch
+lunch infinity_Pacman-userdebug
+
+# Build
 m bacon
-echo "✅ Build finished!"
