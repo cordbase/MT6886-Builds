6e545693a9ae7c3ae2250113a379343e5b2fe62b
CordBase <144811064+cordbase@users.noreply.github.com>
2025-10-02 00:48:03 +0530

    Update infinityx_pacman.sh

--- infinityx_pacman.sh
+++ infinityx_pacman.sh
@@ -17,7 +17,7 @@ rm -rf kernel/nothing/mt6886-modules
 repo init --no-repo-verify --git-lfs -u https://github.com/ProjectInfinity-X/manifest -b 16 -g default,-mips,-darwin,-notdefault
 
 # Device Bringup
-curl -L https://raw.githubusercontent.com/cordbase/MT6886-Builds/main/infinityx.xml -o .repo/local_manifests/local_manifests.xml
+git clone https://github.com/cordbase/local_manifest --depth=1 .repo/local_manifests
 
 # Sync the repositories  
 /opt/crave/resync.sh 
