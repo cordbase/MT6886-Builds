1d9dfa665b0efdf8100a948301dd6c58718bec44
CordBase <144811064+cordbase@users.noreply.github.com>
2025-10-06 22:53:00 +0530

    Update pos15_pacman.sh

--- pos15_pacman.sh
+++ pos15_pacman.sh
@@ -8,6 +8,7 @@ rm -rf prebuilts/clang/host/linux-x86
 repo init -u https://github.com/PixelOS-AOSP/manifest.git -b fifteen --git-lfs
 
 # Device Manifest
+git clone https://github.com/cordbase/local_manifest.git -b pos .repo/local_manifests
 
 # Sync the repositories  
 /opt/crave/resync.sh
